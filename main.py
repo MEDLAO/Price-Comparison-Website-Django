0d2ac2d582df6aeb586d37e487c9771c37674511
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-01-11 22:52:55 +0100

    get all products for a page

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-amazon_product_url = "https://www.amazon.eg/-/en/?language=en_AE"
+amazon_product_url = "https://www.amazon.eg/s?k=smart+watch&ref=nb_sb_noss"
 
 # populate headers
-headers = {"user-agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) "
-           "Chrome/120.0.0.0 Safari/537.36"}
+headers = {"user-agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36"
+                        " (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
 
 # Fetching data and cleaning it
-
 page = requests.get(url=amazon_product_url, headers=headers)
 soup = BeautifulSoup(page.content, 'lxml')
-print(soup.prettify())
+#print(soup.prettify())
+
+products = soup.find_all('div', class_='a-section a-spacing-base')
+print(products)
