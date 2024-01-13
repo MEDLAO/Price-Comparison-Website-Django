b1c179f666a096191df1032b003e41f481a22ce3
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-01-13 23:56:02 +0100

    got cleaned up price and description

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+
 
 amazon_product_url = "https://www.amazon.eg/s?k=smart+watch&ref=nb_sb_noss"
 
@@ -13,6 +15,18 @@ soup = BeautifulSoup(page.content, 'lxml')
 #print(soup.prettify())
 
 products = soup.find_all('div', class_='a-section a-spacing-base')
-images = soup.find_all('img', class_="s-image")
-# prices = soup.find_all('span', attrs={"aria-hidden": "true"})
-print(products)
+
+for product in products:
+    # price
+    price_with_html_tag = product.find('span', class_='a-offscreen')
+    if price_with_html_tag:
+        price = price_with_html_tag.get_text()
+        price = re.sub(r'جنيه', '', price)
+        print(price)
+
+    # description
+    description_with_html_tag = product.find('span', class_='a-size-base-plus a-color-base '
+                                                            'a-text-normal')
+    description = description_with_html_tag.get_text()
+    print(description)
+
