b7ff6ed08d3a2f89ef039fffaf4734a5bcf0ab46
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-01-14 00:47:49 +0100

    add product url + image

--- main.py
+++ main.py
@@ -17,6 +17,16 @@ soup = BeautifulSoup(page.content, 'lxml')
 products = soup.find_all('div', class_='a-section a-spacing-base')
 
 for product in products:
+
+    # brand
+
+    # class ="a-size-mini s-line-clamp-1" > < span class ="a-size-base-plus a-color-base" > KOSPET < / span > < / h2 >
+    h2 = product.find('h2', class_='a-size-mini s-line-clamp-1')
+    print(h2)
+
+    # brand = h2.find_all('span')
+    # print(brand)
+
     # price
     price_with_html_tag = product.find('span', class_='a-offscreen')
     if price_with_html_tag:
@@ -26,7 +36,16 @@ for product in products:
 
     # description
     description_with_html_tag = product.find('span', class_='a-size-base-plus a-color-base '
-                                                            'a-text-normal')
+                                                        'a-text-normal')
     description = description_with_html_tag.get_text()
     print(description)
 
+    # image
+    image_with_html_tag = product.find('img', class_='s-image')
+    image = image_with_html_tag.attrs['src']
+    print(image)
+
+    # link
+    link_with_html_tag = product.find('a', class_='a-link-normal s-no-outline')
+    link = "https://www.amazon.eg" + link_with_html_tag.attrs['href']
+    print(link)
