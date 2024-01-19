8bbf39d76f64be79d0a7134ee20f8526a90a95d0
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-01-19 21:25:31 +0100

    proxies and user-agents rotators

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import re
 import pandas as pd
 from io import StringIO
+import random
 
 
 amazon_product_url_ar = "https://www.amazon.eg/s?k=smart+watch&language=ar_AE&crid=VEA91KM5TLZU&" \
@@ -10,24 +11,49 @@ amazon_product_url_ar = "https://www.amazon.eg/s?k=smart+watch&language=ar_AE&cr
 amazon_product_url_en = "https://www.amazon.eg/s?k=smart+watch&language=en_AE&crid=VEA91KM5TLZU&" \
                         "sprefix=%2Caps%2C158&ref=nb_sb_ss_recent_1_0_recent"
 
+# Rotating user-agents
+user_agent_list = [
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36" (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+]
+
+random_user_agent = random.choice(user_agent_list)
+
 # populate headers
-headers = {"user-agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36"
-                        " (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+headers = {"user-agent":random_user_agent,
            #"Accept-Language": "ar-ae,en-ae"
            }
 
 # Rotating proxies
 
 # parse the free proxies form the HTML
-prox_list = []
+proxy_list = []
 response = requests.get('https://free-proxy-list.net/')
 df = pd.read_html(StringIO(str(response.text)))[0]
 for ip, port in zip(df['IP Address'], df['Port']):
-    prox_list.append(ip + ':' + str(port))
-print(prox_list)
+    proxy_list.append(ip + ':' + str(port))
+print(proxy_list)
+
+working_proxies = []
+for i in proxy_list[:10]:
+    try:
+        proxy = {'http':'http://' + i,
+                 'https':'https://' + i}
+        res = requests.get('http://example.org/', proxies = proxy)
+        working_proxies.append(i)
+    except:
+        pass
+
+print(working_proxies)
+
 
 # Fetching data and cleaning it
-page = requests.get(url=amazon_product_url_ar, headers=headers)
+random_proxy = random.choice(working_proxies)
+page = requests.get(url=amazon_product_url_ar, headers=headers, proxies = random_proxy)
 soup = BeautifulSoup(page.content, 'lxml')
 #print(soup.prettify())
 
