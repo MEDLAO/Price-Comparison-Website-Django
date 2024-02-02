6d2487f35c6281d43b743ba681543f5e8b1f1be9
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-02-02 20:47:36 +0100

    format proxy

--- main3.py
+++ main3.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 from random import randint
 from time import sleep
+from fake_useragent import UserAgent
 
 
 response = requests.get("https://sslproxies.org/")
@@ -20,7 +21,8 @@ print(ips_with_ports)
 def proxy_generator():
     response = requests.get("https://sslproxies.org/")
     soup = BeautifulSoup(response.content, 'html.parser')
-    proxy = {'https': choice(ips_with_ports)}
+    choosen_proxy = choice(ips_with_ports)
+    proxy = {'https': choosen_proxy}
     return proxy
 
 def check_proxy(url, **kwargs):
@@ -28,9 +30,10 @@ def check_proxy(url, **kwargs):
         try:
             proxy = proxy_generator()
             print("Proxy currently being used: {}".format(proxy))
-            response = requests.get(url, proxies=proxy, timeout=7, **kwargs)
+            response = requests.get(url, proxies = proxy, timeout = 7, **kwargs)
             print(response.status_code)
             print(response.text)
+            proxy['https'] = f"https://{proxy['https']}"
             return proxy
             # if the request is successful, no exception is raised
         except:
@@ -42,12 +45,17 @@ def check_proxy(url, **kwargs):
 
 
 valid_proxy = check_proxy("https://httpbin.org/ip")
+print(valid_proxy)
 
 
 def data_scraper(url_scrap):
     # add user_agent
+    ua = UserAgent()
+    headers = {"user-agent": ua.random}
+
     # fetch the html page with a http get request
-    response = requests.get(url_scrap, proxies = valid_proxy)
+    response = requests.get(url_scrap, headers = headers, proxies = valid_proxy)
+    # proxies = valid_proxy
 
     # parse the html content of the page
     soup = BeautifulSoup(response.content, "html.parser")
