e0291710d6f5db98cf52662355e4f8e1313c2782
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-02-23 20:24:18 +0100

    started implementing code for asynchronous web scraping

--- main.py
+++ main.py
@@ -1,70 +1,19 @@
+import asyncio
+import aiohttp as aiohttp
 import requests
 from bs4 import BeautifulSoup
-import re
-from requests_ip_rotator import ApiGateway
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-ACCESS_KEY_SECRET = os.getenv('ACCESS_KEY_SECRET')
-
-api_url = "https://amazon.eg"
-amazon_product_url_ar = api_url + "/s?k=smart+watch&crid=2VO6ZUHG1QLMC&sprefix=smart%2Caps%2C149&ref=nb_sb_ss_ts-doa-p_1_5"
-amazon_product_url_en = api_url + "/s?k=smart+watch&language=en"
+HOME_URL = "https://amazon.eg"
+AMAZON_PRODUCT_URL_AR = "https://www.amazon.eg/s?bbn=18018102031&rh=n%3A21832958031&fs=true&language=ar_AE&ref=lp_21832958031_sar"
+AMAZON_PRODUCT_URL_EN = "https://www.amazon.eg/s?bbn=18018102031&rh=n%3A21832958031&fs=true&language=en_AE&ref=lp_21832958031_sar"
 
 
-# Create gateway object and initialise in
-# AWS API Gateway's large IP pool as a proxy to generate pseudo-infinite IPs
-gateway = ApiGateway(api_url, access_key_id = AWS_ACCESS_KEY_ID, access_key_secret = ACCESS_KEY_SECRET)
-gateway.start()
+async def scrape(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    products = soup.find_all('div', class_='a-section a-spacing-base')
+    print(products[0])
 
-# Assign gateway to session
-session = requests.Session()
-session.mount(api_url, gateway)
 
-url = api_url
-
-# Send request (IP will be randomised)
-response = session.get(url)
-print(response.status_code)
-
-# Fetching data and cleaning it
-soup = BeautifulSoup(response.content, 'lxml')
-print(soup.prettify())
-
-products = soup.find_all('div', class_='a-section a-spacing-base')
-
-for product in products:
-    # price
-    price_with_html_tag = product.find('span', class_='a-offscreen')
-    if price_with_html_tag:
-        price = price_with_html_tag.get_text()
-        price = re.sub(r'جنيه', '', price)
-        #print(price)
-
-    # description
-    description_with_html_tag = product.find('span', class_='a-size-base-plus a-color-base '
-                                                        'a-text-normal')
-    description = description_with_html_tag.get_text()
-    #print(description)
-
-    # image
-    image_with_html_tag = product.find('img', class_='s-image')
-    image = image_with_html_tag.attrs['src']
-    #print(image)
-
-    # link
-    link_with_html_tag = product.find('a', class_='a-link-normal s-no-outline')
-    link = "https://www.amazon.eg" + link_with_html_tag.attrs['href']
-    #print(link)
-
-    # next_button_with_html_tags = soup.div('a', class_='s-pagination-item s-pagination-next s-pagination-button s-pagination-separator')
-    # next_button = next_button_with_html_tags[0].attrs['href']
-
-# Delete gateways
-gateway.shutdown()
-
-# 3rd approach : use selenium to search 'smart watch' in the search bar.
+asyncio.run(scrape(AMAZON_PRODUCT_URL_AR))
--- main3.py
+++ main3.py
@@ -100,35 +100,35 @@ def data_scraper(url_scrap):
         print(products[0])
 
         final_products = [] # creating a list of dictionaries
-        for product in products:
-            final_product = {} # creating a dictionary to store the product information
-
-            # price
-            price_with_html_tag = product.find('span', class_='a-offscreen')
-            if price_with_html_tag:
-                price = price_with_html_tag.get_text()
-                price = re.sub(r'جنيه', '', price)
-                final_product["price"] = price
-                # print(price)
-
-            # description
-            description_with_html_tag = product.find('span', class_='a-size-base-plus a-color-base '
-                                                                    'a-text-normal')
-            description = description_with_html_tag.get_text()
-            final_product["description"] = description
-            # print(description)
-
-            # image
-            image_with_html_tag = product.find('img', class_='s-image')
-            image = image_with_html_tag.attrs['src']
-            final_product["image"] = image
-            # print(image)
-
-            # link
-            link_with_html_tag = product.find('a', class_='a-link-normal s-no-outline')
-            link = "https://www.amazon.eg" + link_with_html_tag.attrs['href']
-            final_product["link"] = link
-            # print(link)
+        # for product in products:
+        #     final_product = {} # creating a dictionary to store the product information
+        #
+        #     # price
+        #     price_with_html_tag = product.find('span', class_='a-offscreen')
+        #     if price_with_html_tag:
+        #         price = price_with_html_tag.get_text()
+        #         price = re.sub(r'جنيه', '', price)
+        #         final_product["price"] = price
+        #         # print(price)
+        #
+        #     # description
+        #     description_with_html_tag = product.find('span', class_='a-size-base-plus a-color-base '
+        #                                                             'a-text-normal')
+        #     description = description_with_html_tag.get_text()
+        #     final_product["description"] = description
+        #     # print(description)
+        #
+        #     # image
+        #     image_with_html_tag = product.find('img', class_='s-image')
+        #     image = image_with_html_tag.attrs['src']
+        #     final_product["image"] = image
+        #     # print(image)
+        #
+        #     # link
+        #     link_with_html_tag = product.find('a', class_='a-link-normal s-no-outline')
+        #     link = "https://www.amazon.eg" + link_with_html_tag.attrs['href']
+        #     final_product["link"] = link
+        #     # print(link)
 
         # # next page
         # next_page_with_tag = soup.find('a', class_ ="s-pagination-item s-pagination-next s-pagination-button s-pagination-separator")
