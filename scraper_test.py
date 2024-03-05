e326e54724293bbb7a13887a61824c4889874ecd
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-03-05 22:59:25 +0100

    test with selenium

--- scraper_test.py
+++ scraper_test.py
@@ -4,11 +4,12 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
-driver.get("https://www.noon.com/egypt-en/search/?q=smart%20watch&page=2")
+driver.get("https://www.amazon.eg/")
 
 get_source = driver.page_source
-products = driver.find_elements(By.CSS_SELECTOR, 'span.productContainer')
-products_text = []
-for product in products:
-    products_text.append(product.text)
-print(products_text)
+# products = driver.find_elements(By.CSS_SELECTOR, 'span.productContainer')
+# products = driver.find_element(By.CLASS_NAME, 'sc-5c17cc27-0 eCGMdH wrapper productContainer')
+# products_text = []
+# for product in products:
+#     products_text.append(product.text)
+print(get_source.text)
