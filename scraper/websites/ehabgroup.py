8e58be4996c818c23d217700f653802a7f73f8b4
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-05-09 20:38:59 +0100

    fixed description problem ehabgroup

--- scraper/websites/ehabgroup.py
+++ scraper/websites/ehabgroup.py
@@ -22,41 +22,38 @@ async def fetch_ehabgroup(s, url, headers):
                 products = soup.find_all("div", class_="product-item-info")
                 print(f'Product for page {url}')
                 for product in products:
-                    a_links = product.find_all("a")
+                    description_with_html_tag = product.find("a", class_="product-item-link")
+                    if description_with_html_tag:
+                        a_links = product.find_all("a")
 
-                    # price
-                    price_with_html_tag = product.find("span", class_="price")
-                    if price_with_html_tag:
-                        price = price_with_html_tag.get_text()
-                        print(price)
+                        # price
+                        price_with_html_tag = product.find("span", class_="price")
+                        if price_with_html_tag:
+                            price = price_with_html_tag.get_text()
+                            print(price)
 
-                    # description
-                    description = a_links[1].get_text()
-                    if description == "Add to Wish List":
-                        description_with_html_tag = product.find("a", class_="product-item-link")
-                        if description_with_html_tag:
-                            description = description_with_html_tag.get_text()
-                            print(description)
-                    print(description)
+                        # description
+                        description = description_with_html_tag.get_text()
+                        print(description)
 
-                    # brand
-                    brand = find_product_attribute(BRANDS_EN, description)
-                    print(brand)
+                        # brand
+                        brand = find_product_attribute(BRANDS_EN, description)
+                        print(brand)
 
-                    # color
-                    color = find_product_attribute(COLORS_EN, description)
-                    print(color)
+                        # color
+                        color = find_product_attribute(COLORS_EN, description)
+                        print(color)
 
-                    # image
-                    image_with_html_tag = product.find("img",
-                                                       class_="product-image-photo hover_image")
-                    if image_with_html_tag:
-                        image = image_with_html_tag.attrs['src']
-                        print(image)
+                        # image
+                        image_with_html_tag = product.find("img",
+                                                           class_="product-image-photo hover_image")
+                        if image_with_html_tag:
+                            image = image_with_html_tag.attrs['src']
+                            print(image)
 
-                    # link
-                    link = a_links[0].attrs['href']
-                    print(link)
+                        # link
+                        link = a_links[0].attrs['href']
+                        print(link)
                 print(headers)
 
         except aiohttp.ClientError:
