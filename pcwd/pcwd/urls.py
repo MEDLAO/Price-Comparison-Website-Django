9e3c96dcd734bd1e0fbf837dc80ed32c52196e46
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-05-30 16:54:59 +0100

    test display EH products

--- pcwd/pcwd/urls.py
+++ pcwd/pcwd/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from product.views import ProductListView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', ProductListView.as_view(), name='product_list'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- pcwd/product/views.py
+++ pcwd/product/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import ScrapedProduct
 
-# Create your views here.
+
+class ProductListView(ListView):
+    model = ScrapedProduct
+    template_name = 'product_list.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        # Fetch all products and ensure translations are loaded
+        return ScrapedProduct.objects.language('en').all()
