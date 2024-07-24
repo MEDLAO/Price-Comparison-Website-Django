5f655d1f87b2bbf335dd80fc8cc14fe9857d7b4d
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-07-24 23:42:56 +0100

    add test_save_method_download_image

--- pcwd/product/tests/test_models.py
+++ pcwd/product/tests/test_models.py
@@ -13,3 +13,11 @@ def test_get_image_upload_path_amazon(scraped_product):
     path = ScrapedProduct.get_image_upload_path(scraped_product, '_AC_SL1500_.jpg')
     assert path == 'amazon/_AC_SL1500_.jpg'
 
+
+@pytest.mark.django_db
+def test_save_method_download_image(scraped_product):
+    # Verify that the image was saved with the correct path
+    assert scraped_product.image.name == 'amazon/image.jpg'
+
+    # Verify that the content of the saved image matches the mock response content
+    assert scraped_product.image.read() == b'test_image_content'
