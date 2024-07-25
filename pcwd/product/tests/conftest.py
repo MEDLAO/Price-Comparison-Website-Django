47055f93c3a4f1b9154ea36f22036cc01121f405
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-07-25 19:22:31 +0100

    update fixtures and model tests

--- pcwd/product/tests/conftest.py
+++ pcwd/product/tests/conftest.py
@@ -9,7 +9,7 @@ def website():
 
 @pytest.fixture
 def scraped_product(website):
-    return ScrapedProduct.objects.create(
+    scraped_product = ScrapedProduct.objects.create(
         website=website,
         product_url="https://www.amazon.eg/Xiaomi-Display-Resistant-Battery-Tracking/dp/B0C7R1RN2D"
                     "/ref=mp_s_a_1_4?crid=3L75S7JNV9KZD&dib=eyJ2IjoiMSJ9.eUPxj3SOeVi8nifQdNTFBthj"
@@ -18,5 +18,51 @@ def scraped_product(website):
                     "T1Uof4Mghv4XG2P5Dmdw8SPsYRRAdaLvo4R3Tdb0Aaw.27WRZzP9eK7D0aNiT5PLaVKlu5pKYK2_"
                     "IxEnGeuct_k&dib_tag=se&keywords=smart%2Bwatches&qid=1721756059&sprefix=smart"
                     "%2Bwa%2Caps%2C599&sr=8-4&th=1",
-        image_url="https://m.media-amazon.com/images/I/615LVMteaYL._AC_SL1500_.jpg"
+        image_url="https://m.media-amazon.com/images/I/615LVMteaYL._AC_SL1500_.jpg",
+        price=2099.00
     )
+
+    # set English translations
+    scraped_product.translations.create(
+        language_code='en',
+        description='English description',
+        brand='Brand A',
+        color='Red',
+        currency='EGP'
+    )
+
+    scraped_product.translation.create(
+        language_code='en',
+        product_url="https://www.amazon.eg/Xiaomi-Display-Resistant-Battery-Tracking/dp/B0C7R1RN2D"
+                    "/ref=mp_s_a_1_4?crid=3L75S7JNV9KZD&dib=eyJ2IjoiMSJ9.eUPxj3SOeVi8nifQdNTFBthj"
+                    "71E0EkWGzaJ0TXsnOJ3WY26F-mXV12xKLRDlgiu_sJ6dTe5KmrWIrZJe8KGyD47vaW5_T-9D1d-"
+                    "vevwtaGvMZDCs5VFA9IFIH98-UgZrg2bDhyxzlHNdxqBuf48nCz9gya6cBekCMu8stNY_vN-"
+                    "T1Uof4Mghv4XG2P5Dmdw8SPsYRRAdaLvo4R3Tdb0Aaw.27WRZzP9eK7D0aNiT5PLaVKlu5pKYK2_"
+                    "IxEnGeuct_k&dib_tag=se&keywords=smart%2Bwatches&qid=1721756059&sprefix=smart"
+                    "%2Bwa%2Caps%2C599&sr=8-4&th=1"
+    )
+
+    # set Arabic translations
+    scraped_product.translations.create(
+        language_code='ar',
+        description='وصف عربي',
+        brand='العلامة التجارية أ',
+        color='أحمر',
+        currency='جنيه'
+    )
+
+    scraped_product.translation.create(
+        language_code='ar',
+        product_url="https://www.amazon.eg/%D8%B4%D8%A7%D9%88%D9%85%D9%8A-%D8%A7%D9%84%D9%85%D9%88"
+                    "%D8%A7%D9%82%D8%B9-%D8%A7%D9%84%D8%B9%D8%A7%D9%84%D9%85%D9%8A-%D8%A7%D9%84%D9"
+                    "%84%D9%8A%D8%A7%D9%82%D8%A9-%D8%A7%D9%84%D8%A8%D8%AF%D9%86%D9%8A%D8%A9/dp/"
+                    "B0C7R1RN2D/ref=sr_1_6?crid=3LI1Y8NP6ZWR9&dib=eyJ2IjoiMSJ9.eUPxj3SOeVi8nifQdNT"
+                    "FBpU729InDij1qnWUROwBhLw3z2Wf2E0YfCQHGUVxZ_OUsqvymnhuHNazS6OqmMvsE89VzwHdsjU2"
+                    "xwu9nf5O-gS9YXIJ-DEKyWWMKcoM-vs8aK9aLWj25KPLQqfzkhoox-lN6gC4R67PnQRkyqSHYDVYU"
+                    "zFxUnLG-UFRn5rqcFsVGEm-KIcpz_f8j33yBugpGXUmgv0XVXWe-T6IynR1IExGoS2Kw_7ZCVERs"
+                    "TEqeQesQpGmGgEN3DBTB_DAoNSFziexFfnEpPdPZ6dDJn__FWc.0jE8XfZWep94jg7pj0mQtHzq0"
+                    "eSud7NNOcZU3oiqUj4&dib_tag=se&keywords=smart%2Bwatch&qid=1721930997&sprefix="
+                    "smart%2Caps%2C579&sr=8-6&th=1"
+    )
+
+    return scraped_product
--- pcwd/product/tests/test_models.py
+++ pcwd/product/tests/test_models.py
@@ -10,14 +10,25 @@ def test_str_method(scraped_product):
 
 @pytest.mark.django_db
 def test_get_image_upload_path_amazon(scraped_product):
-    path = ScrapedProduct.get_image_upload_path(scraped_product, '_AC_SL1500_.jpg')
-    assert path == 'amazon/_AC_SL1500_.jpg'
+    path = ScrapedProduct.get_image_upload_path(scraped_product, '615LVMteaYL._AC_SL1500_.jpg')
+    assert path == 'amazon/615LVMteaYL._AC_SL1500_.jpg'
 
 
 @pytest.mark.django_db
-def test_save_method_download_image(scraped_product):
-    # Verify that the image was saved with the correct path
-    assert scraped_product.image.name == 'amazon/image.jpg'
+def test_save_method_download_image(mocker, scraped_product):
+    # create a mock response object
+    mock_response = mocker.Mock()
+    mock_response.status_code = 200
+    mock_response.content = b'image_content'
 
-    # Verify that the content of the saved image matches the mock response content
+    # patch 'requests.get' in the models module to return the mock response when called
+    mocker.patch('.models.requests.get', return_value=mock_response)
+
+    # verify that the image was saved with the correct path
+    assert scraped_product.image.name == 'amazon/615LVMteaYL._AC_SL1500_.jpg'
+
+    # verify that the content of the saved image matches the mock response content
     assert scraped_product.image.read() == b'test_image_content'
+
+
+
