8b22f6fb2575ef2a49e964212aad0a8b7cce1db7
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-07-31 20:21:15 +0100

    remove query parameter from test_product_list_view

--- pcwd/product/tests/test_views.py
+++ pcwd/product/tests/test_views.py
@@ -7,18 +7,15 @@ from .models import ScrapedProduct
 
 
 @pytest.mark.django_db
-def test_product_list_view_search_functionality(client, scraped_product):
-    # define the path and send a GET request with a search query
+def test_product_list_view(client, scraped_product):
+    # define the path and send a GET request
     path = reverse('product-list-en')
-    response = client.get(path, {'q': 'Brand'})
+    response = client.get(path)
 
     # decode the response content
     content = response.content.decode()
-
-    # expected content (simplified for demonstration)
     expected_content = "English description"
 
-    # perform assertions
     assert expected_content in content
     assert response.status_code == 200
     assertTemplateUsed(response, 'product_list_en.html')
