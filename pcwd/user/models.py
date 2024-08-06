013d892889f4aa32b65040d9894aad72170fb296
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-08-06 21:18:25 +0100

    define CustomUserManager

--- pcwd/user/models.py
+++ pcwd/user/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import import AbstractUser, BaseUserManager, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from .validators import validate_username, validate_email
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The email field must be set')
+        if not username:
+            raise ValueError('The username field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, username, password, **extra_fields)
+
+
+
 
