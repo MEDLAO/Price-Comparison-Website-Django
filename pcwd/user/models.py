8f3c423d45b58ef523959ee7248afa3de1ddb108
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-08-22 21:28:56 +0100

    fix issue with CustomUser

--- pcwd/user/models.py
+++ pcwd/user/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
-from user.validators import validate_username, validate_email
+from .validators import validate_username, validate_email
 
 
 class CustomUserManager(BaseUserManager):
--- pcwd/user/validators.py
+++ pcwd/user/validators.py
@@ -2,14 +2,13 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 
 
-User = get_user_model()
-
-
 def validate_username(value):
-    if User.objects.filter(email=value).exists():
+    user = get_user_model()
+    if user.objects.filter(email=value).exists():
         raise ValidationError('This username is already used as an email by another user.')
 
 
 def validate_email(value):
-    if User.objects.filter(username=value).exists():
+    user = get_user_model()
+    if user.objects.filter(username=value).exists():
         raise ValidationError('This email is already used as a username by another user.')
