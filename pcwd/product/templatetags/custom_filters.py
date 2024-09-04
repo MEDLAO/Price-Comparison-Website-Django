1a6e0fec06fcf9487baa3b9cc9362dd3abe10401
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-09-04 16:53:15 +0100

    error with the custom filter in template

--- pcwd/product/templatetags/custom_filters.py
+++ pcwd/product/templatetags/custom_filters.py
@@ -9,3 +9,17 @@ def add(value, arg):
         return int(value) + int(arg)
     except (ValueError, TypeError):
         return ''
+
+
+@register.filter
+def get_by_id(queryset, product_id):
+    return queryset.get(id=product_id)
+
+
+@register.filter
+def dict_get(dictionary, key):
+    """
+    Custom template filter to get a value from a dictionary using a key.
+    Returns an empty list if the key is not found.
+    """
+    return dictionary.get(key, [])
