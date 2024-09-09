17551205985bb5014e58b27da41e06a152abd245
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-09-09 18:29:07 +0100

    add unit tests for user.models

---
+++ pcwd/user/tests/test_models.py
@@ -0,0 +1,31 @@
+import pytest
+from django.contrib.auth import get_user_model
+from .models import Profile
+
+
+User = get_user_model()
+
+
+@pytest.mark.django_db
+def test_create_user(create_test_user):
+    """Test that a regular user can be created."""
+    user = create_test_user
+    assert user.email == "testuser@abc.com"
+    assert user.username == "username_test"
+    assert user.check_password("pwabcde8") is True
+
+
+@pytest.mark.django_db
+def test_create_superuser(create_test_superuser):
+    """Test that a superuser can be created."""
+    superuser = create_test_superuser
+    assert superuser.is_superuser is True
+    assert superuser.is_staff is True
+
+
+@pytest.mark.django_db
+def test_profile_creation(create_test_user):
+    """Test that a profile is created automatically when a user is created."""
+    user = create_test_user
+    assert Profile.objects.filter(user=user).exists()
+
