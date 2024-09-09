2011297b858ca33d47af751fba68d3ff65193f51
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-09-09 18:10:04 +0100

    add fixtures for authentication unit tests

---
+++ pcwd/user/tests/conftest.py
@@ -0,0 +1,32 @@
+import pytest
+from django.test import Client
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+@pytest.fixture
+def client():
+    return Client()
+
+
+@pytest.fixture
+def create_test_user():
+    """Fixture to create a regular test user."""
+    return User.objects.create_user(
+        email="testuser@abc.com",
+        username="username_test",
+        password="pwabcde8"
+    )
+
+
+@pytest.fixture
+def create_test_superuser():
+    """Fixture to create a superuser."""
+    return User.objects.create_superuser(
+        email="superuser@abc.com",
+        username="superuser_username__test",
+        password="pwadmin8"
+    )
+
