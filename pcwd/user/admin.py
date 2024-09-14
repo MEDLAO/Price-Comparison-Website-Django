5410ed12b32928ce0e147ab8ac9f6570e68a5e7c
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-09-14 22:04:49 +0100

    register the user models for admin interface

--- pcwd/user/admin.py
+++ pcwd/user/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Profile
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    # Display these fields in the admin user list
+    list_display = ('email', 'username', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+
+    # Fields to be displayed when viewing a user in the admin
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        (_('Permissions'), {'fields': ('is_staff', 'is_active', 'is_superuser')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    # Fields to be displayed when adding a new user in the admin
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'is_staff', 'is_active', 'is_superuser'),
+        }),
+    )
+
+    # Allow searching for users by email and username
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+
+# Register the CustomUser model with the custom admin
+admin.site.register(CustomUser, CustomUserAdmin)
+
+
+# Register the Profile model in the admin
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'image')
+    search_fields = ('user__username', 'user__email')
+
