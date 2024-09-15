27da6bfd4886e8eefc2a7070265e95b03f5e54cc
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-09-15 11:56:18 +0100

    corrections in user/admin.py

--- pcwd/user/admin.py
+++ pcwd/user/admin.py
@@ -1,22 +1,21 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Profile
-from django.utils.translation import gettext_lazy as _
 
 
 class CustomUserAdmin(UserAdmin):
-    # Display these fields in the admin user list
+    # display these fields in the admin user list
     list_display = ('email', 'username', 'is_staff', 'is_active')
     list_filter = ('is_staff', 'is_active', 'is_superuser')
 
-    # Fields to be displayed when viewing a user in the admin
+    # fields to be displayed when viewing a user in the admin
     fieldsets = (
         (None, {'fields': ('email', 'username', 'password')}),
-        (_('Permissions'), {'fields': ('is_staff', 'is_active', 'is_superuser')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
 
-    # Fields to be displayed when adding a new user in the admin
+    # fields to be displayed when adding a new user in the admin
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
@@ -24,17 +23,17 @@ class CustomUserAdmin(UserAdmin):
         }),
     )
 
-    # Allow searching for users by email and username
+    # allow searching for users by email and username
     search_fields = ('email', 'username')
     ordering = ('email',)
 
-# Register the CustomUser model with the custom admin
+
+# register the CustomUser model with the custom admin
 admin.site.register(CustomUser, CustomUserAdmin)
 
 
-# Register the Profile model in the admin
+# register the Profile model in the admin
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'image')
     search_fields = ('user__username', 'user__email')
-
