3e7cb02126541bfeecb8bdc017768291081eccd1
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-09-16 17:57:09 +0100

    start to implement feed

---
+++ pcwd/feed/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import PostListView, PostCreateView
+
+
+app_name = 'feed'
+
+urlpatterns = [
+    path('feed/', PostListView.as_view(), name='post-list'),
+    path('post_form/', PostCreateView.as_view(), name='post-create')
+]
+
--- pcwd/feed/views.py
+++ pcwd/feed/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+# listView to display all the posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'feed/feed.html'
+    context_object_name = 'posts'
+    ordering = ['-created_at']
+    paginate_by = 10
+
+
+# createView to handle post submissions
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['content']
+    template_name = 'feed/post_form.html'
+    success_url = reverse_lazy('feed:post-list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
