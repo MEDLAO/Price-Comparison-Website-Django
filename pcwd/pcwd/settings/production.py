b4cda2d8a58d7f320b5186541a9b03a888d99f4a
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-10-18 16:03:59 +0100

    replace env() with os.getenv() in production.py

--- pcwd/pcwd/settings/production.py
+++ pcwd/pcwd/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('PROD_SECRET_KEY')
+SECRET_KEY = os.getenv('PROD_SECRET_KEY')
 
 # Debug settings for production
 DEBUG = False
@@ -15,11 +15,11 @@ ALLOWED_HOSTS = ['fromsifr.com', 'www.fromsifr.com', 'ec2-35-180-205-132.eu-west
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('PROD_DB_NAME'),
-        'USER': env('PROD_DB_USER'),
-        'PASSWORD': env('PROD_DB_PASSWORD'),
-        'HOST': env('PROD_DB_HOST'),
-        'PORT': env('PROD_DB_PORT'),
+        'NAME': os.getenv('PROD_DB_NAME'),
+        'USER': os.getenv('PROD_DB_USER'),
+        'PASSWORD': os.getenv('PROD_DB_PASSWORD'),
+        'HOST': os.getenv('PROD_DB_HOST'),
+        'PORT': os.getenv('PROD_DB_PORT'),
         'OPTIONS': {
             'options': '-c search_path=public',
         },
@@ -31,11 +31,11 @@ MEDIA_ROOT = '/app/media'
 
 # AWS SES email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = env('AWS_SES_EMAIL_HOST')
-EMAIL_PORT = env('AWS_SES_PORT')  # TLS port
+EMAIL_HOST = os.getenv('AWS_SES_EMAIL_HOST')
+EMAIL_PORT = os.getenv('AWS_SES_PORT')  # TLS port
 EMAIL_USE_TLS = True  # use TLS for secure email
-EMAIL_HOST_USER = env('AWS_SES_SMTP_USER')
-EMAIL_HOST_PASSWORD = env('AWS_SES_SMTP_PASSWORD')
+EMAIL_HOST_USER = os.getenv('AWS_SES_SMTP_USER')
+EMAIL_HOST_PASSWORD = os.getenv('AWS_SES_SMTP_PASSWORD')
 DEFAULT_FROM_EMAIL = 'contact@fromsifr.com'
 
 CACHES = {
