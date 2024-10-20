1949c574aa667b4c03220a1a05f45054e6dff117
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-10-20 17:41:42 +0100

    add docker-entrypoint.sh to load AWS SSM parameters

---
+++ pcwd/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Fetch AWS SSM parameters by path (/myapp/), iterate over each parameter name
+for param in $(aws ssm get-parameters-by-path --path /myapp/ --with-decryption --query 'Parameters[*].Name' --output text); do
+
+  # Remove the "/pcwd/" prefix from the parameter name to use it as the environment variable key
+  key=$(echo $param | sed 's/\/pcwd\///');
+
+  # Fetch the parameter value using its name, decrypting it if necessary
+  value=$(aws ssm get-parameter --name "$param" --with-decryption --query 'Parameter.Value' --output text);
+
+  # Check if the value is empty; if so, print an error and exit the script
+  if [ -z "$value" ]; then
+    echo "Error: Missing value for $key from AWS SSM"
+    exit 1
+  fi
+
+  # Export the parameter as an environment variable using the key-value pair
+  export $key="$value";
+done
+
