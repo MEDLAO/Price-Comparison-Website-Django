cca2e5e798375d455e84df623d65e18cdb233a85
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-10-23 10:24:36 +0100

    correct get_env_or_ssm() function

--- pcwd/pcwd/settings/utils.py
+++ pcwd/pcwd/settings/utils.py
@@ -17,6 +17,10 @@ def get_ssm_parameter(name, with_decryption=True):
 def get_env_or_ssm(var_name, ssm_param_name=None, default=None):
     """Fetch from environment variable or AWS SSM if not found in env."""
     value = os.getenv(var_name)
-    if not value and ssm_param_name:
+    if value:
+        print(f"Using environment variable for {var_name}")
+        return value or default
+    else:
+        print(f"Environment variable {var_name} not set. Fetching from SSM for {ssm_param_name}")
         value = get_ssm_parameter(ssm_param_name)
-    return value or default
+        return value or default
