b06cf62a718bcd120a9c4a561e695d1e0f326c13
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-10-23 12:54:14 +0100

    edit settings for AWS SSM parameters

--- pcwd/pcwd/settings/production.py
+++ pcwd/pcwd/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 from .utils import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_env_or_ssm('PROD_SECRET_KEY', '/pcwd/PROD_SECRET_KEY')
+SECRET_KEY = get_ssm_parameter('/pcwd/PROD_SECRET_KEY')
 
 # Debug settings for production
 DEBUG = False
@@ -15,11 +15,11 @@ ALLOWED_HOSTS = ['fromsifr.com', 'www.fromsifr.com', 'ec2-35-180-205-132.eu-west
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': get_env_or_ssm('PROD_DB_NAME', '/pcwd/PROD_DB_NAME'),
-        'USER': get_env_or_ssm('PROD_DB_USER', '/pcwd/PROD_DB_USER'),
-        'PASSWORD': get_env_or_ssm('PROD_DB_PASSWORD', '/pcwd/PROD_DB_PASSWORD'),
-        'HOST': get_env_or_ssm('PROD_DB_HOST', '/pcwd/PROD_DB_HOST'),
-        'PORT': get_env_or_ssm('PROD_DB_PORT', '/pcwd/PROD_DB_PORT'),
+        'NAME': get_ssm_parameter('/pcwd/PROD_DB_NAME'),
+        'USER': get_ssm_parameter('/pcwd/PROD_DB_USER'),
+        'PASSWORD': get_ssm_parameter('/pcwd/PROD_DB_PASSWORD'),
+        'HOST': get_ssm_parameter('/pcwd/PROD_DB_HOST'),
+        'PORT': get_ssm_parameter('/pcwd/PROD_DB_PORT'),
         'OPTIONS': {
             'options': '-c search_path=public',
         },
@@ -31,20 +31,20 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # AWS SES email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = get_env_or_ssm('AWS_SES_EMAIL_HOST', '/pcwd/AWS_SES_EMAIL_HOST')
-EMAIL_PORT = get_env_or_ssm('AWS_SES_PORT', '/pcwd/AWS_SES_PORT')  # TLS port
+EMAIL_HOST = get_ssm_parameter('/pcwd/AWS_SES_EMAIL_HOST')
+EMAIL_PORT = get_ssm_parameter('/pcwd/AWS_SES_PORT')  # TLS port
 EMAIL_USE_TLS = True  # use TLS for secure email
-EMAIL_HOST_USER = get_env_or_ssm('AWS_SES_SMTP_USER', '/pcwd/AWS_SES_SMTP_USER')
-EMAIL_HOST_PASSWORD = get_env_or_ssm('AWS_SES_SMTP_PASSWORD', '/pcwd/AWS_SES_SMTP_PASSWORD')
+EMAIL_HOST_USER = get_ssm_parameter('/pcwd/AWS_SES_SMTP_USER')
+EMAIL_HOST_PASSWORD = get_ssm_parameter('/pcwd/AWS_SES_SMTP_PASSWORD')
 DEFAULT_FROM_EMAIL = 'contact@fromsifr.com'
 
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': f"redis://:{get_env_or_ssm('AWS_REDIS_PASSWORD', '/pcwd/AWS_REDIS_PASSWORD')}@{get_env_or_ssm('AWS_REDIS_ENDPOINT', '/pcwd/AWS_REDIS_ENDPOINT')}/1",
+        'LOCATION': f"redis://:{get_ssm_parameter('/pcwd/AWS_REDIS_PASSWORD')}@{get_ssm_parameter('/pcwd/AWS_REDIS_ENDPOINT')}/1",
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-            'PASSWORD': get_env_or_ssm('AWS_REDIS_PASSWORD', '/pcwd/AWS_REDIS_PASSWORD'),
+            'PASSWORD': get_ssm_parameter('/pcwd/AWS_REDIS_PASSWORD'),
         },
     }
 }
