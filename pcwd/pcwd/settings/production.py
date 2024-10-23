07a79e7bba8accab909b8baf5e6c7e777692e235
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-10-23 15:19:43 +0100

    move get_ssm_parameter() from utils.py to production.py for AWS credentials

--- pcwd/pcwd/settings/production.py
+++ pcwd/pcwd/settings/production.py
@@ -1,5 +1,28 @@
 from .base import *
-from .utils import *
+import boto3
+from botocore.exceptions import ClientError
+
+
+def get_ssm_parameter(name, with_decryption=True):
+    """Fetch a parameter from AWS SSM Parameter Store."""
+    aws_access_key = os.getenv('AWS_ACCESS_KEY_ID')
+    aws_secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+    # create a boto3 SSM client using the credentials from the environment variables
+    ssm = boto3.client(
+        'ssm',
+        region_name='eu-west-3',  # Use your specific AWS region
+        aws_access_key_id=aws_access_key,
+        aws_secret_access_key=aws_secret_key
+    )
+
+    try:
+        parameter = ssm.get_parameter(Name=name, WithDecryption=with_decryption)
+        return parameter['Parameter']['Value']
+    except ClientError as e:
+        print(f"Error fetching {name} from SSM: {e}")
+        return None
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_ssm_parameter('/pcwd/PROD_SECRET_KEY')
