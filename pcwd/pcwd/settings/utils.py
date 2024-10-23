27f8ba54c4a3c3457f69fdb49edbcc610b693342
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-10-23 12:26:31 +0100

    edit Dockerfile for AWS credentials

--- pcwd/pcwd/settings/utils.py
+++ pcwd/pcwd/settings/utils.py
@@ -5,21 +5,8 @@ import os
 
 def get_ssm_parameter(name, with_decryption=True):
     """Fetch a parameter from AWS SSM Parameter Store."""
-    aws_access_key = os.getenv('AWS_ACCESS_KEY_ID')
-    aws_secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-
-    # Check if the AWS credentials are available (for build job)
-    if aws_access_key and aws_secret_key:
-        # Use the explicit credentials provided in the environment
-        ssm = boto3.client(
-            'ssm',
-            region_name='eu-west-3',
-            aws_access_key_id=aws_access_key,
-            aws_secret_access_key=aws_secret_key
-        )
-    else:
-        # Assume the instance IAM role (for deploy job on EC2)
-        ssm = boto3.client('ssm', region_name='eu-west-3')
+    # Boto3 will automatically use the credentials from ~/.aws/credentials or IAM role
+    ssm = boto3.client('ssm', region_name='eu-west-3')
 
     try:
         parameter = ssm.get_parameter(Name=name, WithDecryption=with_decryption)
